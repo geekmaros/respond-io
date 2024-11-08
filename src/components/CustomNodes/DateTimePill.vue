644753e4ffc19db6a6c19076519b243ff042b009
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-11-08 04:19:50 +0100

    feat: add datetimeconnector node

--- src/components/CustomNodes/DateTimePill.vue
+++ src/components/CustomNodes/DateTimePill.vue
@@ -1,24 +1,18 @@
 <template>
-  <BaseNode
-    :id="id"
-    :data="data"
-    :selected="selected"
-    :icon="BoltIcon"
-    :title="data.label || 'Trigger'"
-    :subtitle="getTriggerType"
-    icon-background-class="bg-purple-100"
-    icon-color-class="text-purple-600"
-    :handles="[{ id: 'bottom', type: 'source', position: 'bottom' }]"
-  >
+  <div :id="id" class="font-semibold rounded" :class="getStylingClass">
     <div class="flex justify-center items-center">
-      <div class="text-xs text-gray-400 text-center py-3 p-4">{{ getLabel }}</div>
+      <div class="text-xs text-center py-2 p-4">{{ getLabel }}</div>
     </div>
-  </BaseNode>
+
+    <!-- Handles -->
+    <Handle type="target" position="top" class="w-3 h-3 !bg-blue-500" />
+    <Handle type="source" position="bottom" class="w-3 h-3 !bg-blue-500" />
+  </div>
 </template>
 
 <script setup>
 import { computed } from 'vue'
-import BaseNode from './BaseNode.vue'
+import { Handle } from '@vue-flow/core'
 
 const props = defineProps({
   id: {
@@ -41,5 +35,11 @@ const getTriggerType = computed(() => {
 
 const getLabel = computed(() => {
   return props.data.label
+})
+
+const getStylingClass = computed(() => {
+  return props.data.connectorType === 'success'
+    ? 'border-2 border-green-700 bg-green-300 text-green-700'
+    : 'border-2 border-red-700 bg-red-300 text-red-700'
 })
 </script>
